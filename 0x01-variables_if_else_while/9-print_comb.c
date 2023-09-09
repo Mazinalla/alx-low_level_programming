06ac75b48a10556b422311c8483add5965c8a0b2
mazinalla <mazinelsadig8@gmail.com>
2023-09-09 08:02:16 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-        int n;
+	int n;
 
-        for (n = 48; n <= 57; n++)
-        {
-                putchar(n);
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
 		if (n == 57)
 		{
 			continue;
 		}
-                putchar(',');
 		putchar(',');
-        }
-        putchar('\n');
-        return (0);
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
 }
