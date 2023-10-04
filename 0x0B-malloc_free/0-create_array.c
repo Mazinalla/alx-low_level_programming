17330621ac63ad7fd4d8f8eaa3d9b3f44069f670
root <mazinelsadig8@gmail.com>
2023-10-04 16:10:26 -0700

    0x0B-1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,13 +11,15 @@
 char *create_array(unsigned int size, char c)
 {
 	char *n = malloc(size);
-	if(size == 0 || n == 0)
+
+	if (size == 0 || n == 0)
+
 	{
-		return 0;
+		return (0);
 	}
-	while(size--)
+	while (size--)
 	{
-		n[size]= c;
+		n[size] = c;
 	}
-	return n;
+	return (n);
 }
