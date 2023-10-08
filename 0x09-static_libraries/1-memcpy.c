df651023844e2fbb21bbe5ffb71d663db36c9d59
root <mazinelsadig8@gmail.com>
2023-10-08 10:53:45 -0700

    libmy.c

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_memcpy -   copy number bytes form adress "from" to adress "to"
+ *@dest: content is to be copied
+ *@src: Source of data to be copied
+ *@n: bytes of the memory
+ *Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+	unsigned int a;
+
+	a = 0;
+	while (a < n)  /*Declaring WHILE*/
+	{
+		*(dest + n) = *(src + n); /*add 1 position dest and src*/
+		a++;
+
+	} /*END WHILE*/
+
+
+	return (dest);
+}
