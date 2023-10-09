798f4eae0279648fb746a8be12b04f91577a02b5
root <mazinelsadig8@gmail.com>
2023-10-09 03:46:27 -0700

    more malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+  * malloc_checked - allocates memory using malloc.
+  * @b: size of memory to be allocated.
+  *
+  * Return: pointer to allocated memory.
+  */
 void *malloc_checked(unsigned int b)
 {
 	void *p = malloc(b);
