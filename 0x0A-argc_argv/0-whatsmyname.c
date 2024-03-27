ef15a5f4c1b4c23f3497e0d5c3ab346f3f72b888
mazinalla <mazinelsadig8@gmail.com>
2024-03-26 18:16:43 -0700

    argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    (void)argc;
-    printf("%s\n", argv[0]);
-    return 0;
+/**
+  * main - the starting point of a C program.
+  * @argc: the count of command-line arguments.
+  * @argv: an array of strings containing the arguments themselves.
+  *
+  * Return: Always 0.
+  */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }
