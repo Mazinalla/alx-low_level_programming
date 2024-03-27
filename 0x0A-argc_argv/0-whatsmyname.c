27f811b23826750f00045b73502db12f053c98e7
mazinalla <mazinelsadig8@gmail.com>
2024-03-27 04:41:23 +0300

    Argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    (void)argc; // Suppress unused parameter warning
+/**
+ * main - the entry point of the program.
+ * @argc: number of arguments.
+ * @argv: array containing the arguments.
+ * 
+ * Return: always 0.
+*/
+
+int main(int argc , char* argv[]) {
+    (void)argc;
     printf("%s\n", argv[0]);
     return 0;
 }
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program.
+ * @argc: number of arguments.
+ * @argv: array that containing the arguments.
+ *
+ * Return: always 0.
+*/
+
+int main(int argc , char* argv[])
+{
+    printf("%i\n", argc);
+    return (0);
+}
