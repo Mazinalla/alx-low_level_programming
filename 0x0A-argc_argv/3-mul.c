bc82a177019df39fdab6bb991266ae5bd67b047c
mazinalla <mazinelsadig8@gmail.com>
2024-03-26 22:12:35 -0700

    argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,21 +5,25 @@
  * main - the intro to the program.
  * @argc: the count of the arguments.
  * @argv: array countaining argument.
- * 
+ *
  * Return: always 0.
 */
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Error\n");
-        return 1;
-    }
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
 
-    int num1 = atoi(argv[1]);  // Convert first argument to integer
-    int num2 = atoi(argv[2]);  // Convert second argument to integer
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    int result = num1 * num2;  // Perform multiplication
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
-    printf("%d\n", result);    // Print the result
-    return 0;
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
 }
