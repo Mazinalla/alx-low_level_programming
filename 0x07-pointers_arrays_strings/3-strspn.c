7b38cbe63fc952d242a89fc3fb9fba06e7dde03a
mazinalla <mazinelsadig8@gmail.com>
2024-03-28 16:01:53 +0300

    pointers

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,25 +1,30 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * _strspn - gets the length of a prefix substring.
- * @s: string.
- * @accept: string.
- * Return:always 0(success).
- */
+unsigned int _strspn(char *s, char *accept) {
+    unsigned int count = 0;
+    int found;
 
-unsigned int _strspn(char *s, char *accept)
-{
-	int x, z, n = 0;
+    // Iterate through each character in s
+    while (*s) {
+        found = 0;
 
-	for (x = 0; s[x] != '\0'; x++)
-	{
-		for (z = 0; accept[z] != '\0'; z++)
-		{
-			if (s[x] == accept[z])
-			{
-				n++;
-			}
-		}
-	}
-	return (n);
+        // Iterate through each character in accept
+        char *a = accept;
+        while (*a) {
+            // If the current character in s matches any character in accept
+            if (*s == *a) {
+                count++; // Increment the count
+                found = 1; // Mark as found
+                break; // Break out of the inner loop
+            }
+            a++; // Move to the next character in accept
+        }
+
+        // If the current character in s is not found in accept, stop
+        if (!found) break;
+
+        s++; // Move to the next character in s
+    }
+
+    return count;
 }
