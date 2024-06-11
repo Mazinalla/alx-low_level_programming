b2bf004292e8a6379ed9cd4801140b3fb7d32f03
mazinalla <mazinelsadig8@gmail.com>
2024-06-11 14:08:53 -0700

    update

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,31 +7,36 @@
  * Return:(0)always.
  */
 
-char *_strstr(char *haystack, char *needle) {
-    char *p1;
-    char *p1Begin;
-    char *p2;
+char *_strstr(char *haystack, char *needle)
+{
+	char *p1;
+	char *p1Begin;
+	char *p2;
 
-    if (!*needle) {
-        return haystack; /* If needle is empty, return the entire haystack */
-    }
-    
-    p1 = haystack;
-    while (*p1) {
-        p1Begin = p1;
-        p2 = needle;
-        
-        while (*p1 && *p2 && (*p1 == *p2)) {
-            p1++;
-            p2++;
-        }
-        
-        if (!*p2) {
-            return p1Begin; /* Found the needle in haystack */
-        }
-        
-        p1 = p1Begin + 1; /* Move to the next character in haystack and try again */
-    }
-    
-    return NULL; /* Needle not found */
+	if (!*needle)
+	{
+		return (haystack); /* If needle is empty, return the entire haystack */
+	}
+
+	p1 = haystack;
+	while (*p1)
+	{
+		p1Begin = p1;
+		p2 = needle;
+
+		while (*p1 && *p2 && (*p1 == *p2))
+		{
+			p1++;
+			p2++;
+		}
+
+		if (!*p2)
+		{
+			return (p1Begin); /* Found the needle in haystack */
+		}
+
+		p1 = p1Begin + 1; /* Move to the next character in haystack and try again */
+	}
+
+	return (NULL); /* Needle not found */
 }
