bbc6ad36f5173f459eafdf3e9fb80d36b3734afc
mazinalla <mazinelsadig8@gmail.com>
2024-06-11 14:03:11 -0700

    update

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,19 +7,31 @@
  * Return:(0)always.
  */
 
-char *_strstr(char *haystack, char *needle)
-{
-	int x, y;
+char *_strstr(char *haystack, char *needle) {
+    char *p1;
+    char *p1Begin;
+    char *p2;
 
-	for (x = 0; needle[x] != '\0'; x++)
-	{
-		for (y = 0; haystack[y] != '\0'; y++)
-		{
-			if (needle[x] == haystack[y])
-			{
-				haystack[y] = needle[x];
-			}
-		}
-	}
-	return (0);
+    if (!*needle) {
+        return haystack; /* If needle is empty, return the entire haystack */
+    }
+    
+    p1 = haystack;
+    while (*p1) {
+        p1Begin = p1;
+        p2 = needle;
+        
+        while (*p1 && *p2 && (*p1 == *p2)) {
+            p1++;
+            p2++;
+        }
+        
+        if (!*p2) {
+            return p1Begin; /* Found the needle in haystack */
+        }
+        
+        p1 = p1Begin + 1; /* Move to the next character in haystack and try again */
+    }
+    
+    return NULL; /* Needle not found */
 }
