2ce88c1b551038dd380f4e63045afb71ccfcfd2e
mazinalla <mazinelsadig8@gmail.com>
2024-06-11 17:47:54 -0700

    update

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,11 @@
-#/bin/bash
+#!/bin/bash
+# This script creates a static library called liball.a from all the .c files in the current directory
+
+# Compile all .c files into .o files
 gcc -c *.c
-ar rc liball.a *.o
+
+# Create the static library liball.a from the .o files
+ar rcs liball.a *.o
+
+# Clean up the .o files (optional)
+rm *.o
